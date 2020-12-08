0bfc62777aaa00ed4e73b54780c04faad93f02a1
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2020-12-08 09:36:24 +0900

    add no001

--- src/no001.rs
+++ src/no001.rs
@@ -1,3 +1,21 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_7_B&lang=ja
+
+fn a(n: i64, t: i64) -> Vec<[i64; 3]> {
+    let n_ = n + 1;
+    let mut s = vec![];
+    for x in 1..n_ {
+        for y in 1..n_ {
+            for z in 1..n_ {
+                if (x < y && y < z) && (x + y + z == t) {
+                    s.push([x, y, z]);
+                }
+            }
+        }
+    }
+    s
+}
+
 fn main() {
-    println!("No 1");
+    println!("{:?}", a(5, 9).len());
+    println!("{:?}", a(0, 0).len());
 }
