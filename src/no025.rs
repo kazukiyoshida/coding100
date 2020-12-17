e3b660fdf23f027ea74554554a86bb083a5add1f
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2020-12-18 00:54:21 +0900

    update no025

--- src/no025.rs
+++ src/no025.rs
@@ -8,7 +8,9 @@ use proconio::marker::Chars;
 use std::cmp;
 use std::collections::HashSet;
 
-fn main() {
+// -----------------------------------------------------------------------------
+
+fn main_old() {
     input! {
         w: usize,
         h: usize,
@@ -73,4 +75,85 @@ fn dfs(
             }
         }
     }
+}
+
+// -----------------------------------------------------------------------------
+
+struct G {
+    n: u32,
+    w: usize,
+    h: usize,
+    map: Vec<Vec<u32>>,
+    memo: Vec<Vec<Option<u32>>>,
+}
+
+impl G {
+    fn dfs(&mut self, r: (usize, usize)) {
+        // 前提
+        if self.map[r.0][r.1] == 0 {
+            return;
+        }
+
+        let rx = r.0 as isize;
+        let ry = r.1 as isize;
+
+        let nexts = vec![
+            (rx - 1, ry + 1),
+            (rx + 0, ry + 1),
+            (rx + 1, ry + 1),
+            (rx + 1, ry + 0),
+            (rx + 1, ry - 1),
+            (rx + 0, ry - 1),
+            (rx - 1, ry - 1),
+            (rx - 1, ry + 0),
+        ];
+
+        for next in nexts {
+            if next.0 >= 0 && next.1 >= 0 && next.0 < self.h as isize && next.1 < self.w as isize {
+                let x = next.0 as usize;
+                let y = next.1 as usize;
+                if self.memo[x][y].is_none() && self.map[x][y] == 1 {
+                    self.memo[x][y] = Some(self.n);
+                    self.dfs((x, y))
+                }
+            }
+        }
+    }
+
+    fn main(&mut self) {
+        for x in (0..self.h) {
+            for y in (0..self.w) {
+                if self.memo[x][y].is_none() {
+                    // 海
+                    if self.map[x][y] == 0 {
+                        self.memo[x][y] = Some(0);
+                    // 陸
+                    } else if self.map[x][y] == 1 {
+                        self.n += 1;
+                        self.memo[x][y] = Some(self.n);
+                        self.dfs((x, y));
+                    }
+                }
+            }
+        }
+        println!("{:?}", self.memo);
+        println!("n = {:?}", self.n);
+    }
+}
+
+fn main() {
+    input! {
+        w: usize,
+        h: usize,
+        map: [[u32;w]; h],
+    }
+
+    G {
+        n: 0,
+        w,
+        h,
+        map,
+        memo: vec![vec![None; w]; h],
+    }
+    .main();
 }
