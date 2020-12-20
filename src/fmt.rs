e5e011036223531c0ce706b0867c7d1307661d70
Kazuki Yoshida <idkphys.030zzz@gmail.com>
2020-12-20 17:39:08 +0900

    update fmt

--- src/fmt.rs
+++ src/fmt.rs
@@ -8,10 +8,21 @@ use proconio::marker::Chars;
 use std::cmp;
 use std::collections::HashSet;
 
+struct G {
+    x: usize,
+}
+
+impl G {
+    fn dfs(&mut self) {}
+    fn main(&mut self) {}
+}
+
 fn main() {
     input! {
-        x: i32,
+        x: usize,
     }
+
+    G { x }.main();
 
     println!("{:?}", x);
 }
